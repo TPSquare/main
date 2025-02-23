f089138da4dbef8b2e91dd60f1bacce0d57ce0a5
ThanhPhu <132542965+TPSquare@users.noreply.github.com>
2025-02-23 07:37:19 +0700

    .

--- cncgnx/script.js
+++ cncgnx/script.js
@@ -1,3 +1,5 @@
+window.onloadeddata = () => alert("Loaded! 2");
+
 const loadAudio = (filename) => new Promise((res, rej) => {
   const audio = new Audio();
   audio.src = `./assets/${filename}.mp3`;
@@ -10,8 +12,8 @@ const audio = [
   await loadAudio("mua"),
   await loadAudio("thay_phap"),
   await loadAudio("em_dung_di"),
+  await loadAudio("phu_hoa")
 ]
-console.log("Loaded!")
 
 audio[0].loop = true;
 audio[1].loop = true;
@@ -22,6 +24,7 @@ audio[1].volume = 0.3;
 audio[2].volume = 0.4;
 audio[3].volume = 0.8;
 audio[4].volume = 0.8;
+audio[5].volume = 0.8;
 
 let cur = 0;
 const play = (key) => {
@@ -31,7 +34,7 @@ const play = (key) => {
   cur = key;
 }
 
-let order = [0, 1, 2, 0, 3, 0, 4, 0];
+let order = [0, 1, 2, 5, 0, 3, 0, 4, 0];
 
 let idx = -1;
 window.onclick = () => play(order[++idx]);
