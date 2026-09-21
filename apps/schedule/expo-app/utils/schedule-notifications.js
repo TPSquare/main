fec6aa4de303632c2072416b558d3eef7e655256
TPSquare <t2p0t0p8@gmail.com>
2026-09-21 15:43:01 +0700

    .

--- apps/schedule/expo-app/utils/schedule-notifications.js
+++ apps/schedule/expo-app/utils/schedule-notifications.js
@@ -17,6 +17,8 @@ async function requestPermissions() {
 }
 
 function getNotifications(schedule, tagsConfig) {
+  const DATE_TYPE = Notifications.SchedulableTriggerInputTypes.DATE;
+  const INTERVAL_TYPE = Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL;
   const notifications = [];
   for (const dateKey in schedule) {
     for (const time in schedule[dateKey]) {
@@ -25,15 +27,16 @@ function getNotifications(schedule, tagsConfig) {
         ? ` tại ${schedule[dateKey][time].place}`
         : "";
       const noteText = schedule[dateKey][time].note ? "\nMở ứng dụng để xem ghi chú" : "";
-      const type = Notifications.SchedulableTriggerInputTypes.DATE;
       const pushNotification = (message, date) =>
         notifications.push({
           content: {
             title: `${message}: ${schedule[dateKey][time].content}`,
             body: `Thời gian ${timeText}${placeText}${noteText}`,
-            sound: true,
+            sound: "notification.wav",
           },
-          trigger: date ? { type, date } : null,
+          trigger: date
+            ? { type: DATE_TYPE, date, channelId: "notification" }
+            : { type: INTERVAL_TYPE, seconds: 1, channelId: "notification" },
         });
 
       const currentDate = new Date(`${dateKey}T${time.split("-")[0]}`);
@@ -59,12 +62,15 @@ function getNotifications(schedule, tagsConfig) {
     }
   }
   notifications.sort((a, b) => {
-    if (a.trigger === null && b.trigger === null) return 0;
-    if (a.trigger === null) return -1;
-    if (b.trigger === null) return 1;
+    const aIsDate = a.trigger.type === DATE_TYPE;
+    const bIsDate = b.trigger.type === DATE_TYPE;
+    if (!aIsDate && !bIsDate) return 0;
+    if (!aIsDate) return -1;
+    if (!bIsDate) return 1;
     return a.trigger.date.getTime() - b.trigger.date.getTime();
   });
-  notifications[49].content.body += "\nMở ứng dụng để cập nhật các thông báo mới";
+  if (notifications[49])
+    notifications[49].content.body += "\nMở ứng dụng để cập nhật các thông báo mới";
   return notifications.slice(0, 50);
 }
 
@@ -73,10 +79,20 @@ async function sendNotifications(notifications) {
     await Notifications.scheduleNotificationAsync(notification);
 }
 
+const setNotificationChannelAsync = async () => {
+  await Notifications.setNotificationChannelAsync("notification", {
+    name: "Chuông thông báo",
+    importance: Notifications.AndroidImportance.HIGH,
+    sound: "notification.wav",
+    vibrationPattern: [0, 100, 100, 100, 100, 100],
+  });
+};
+
 export default function syncScheduleNotifications(schedule, tagsConfig) {
   useEffect(() => {
     (async () => {
       await requestPermissions();
+      if (Platform.OS === "android") await setNotificationChannelAsync();
       const notifications = getNotifications(schedule, tagsConfig);
       await Notifications.cancelAllScheduledNotificationsAsync();
       await sendNotifications(notifications);
